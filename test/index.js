aa0f66103864e3b1d95cca5637419d60ecdd095c
lebron8dong <73160333+lebron8dong@users.noreply.github.com>
2023-02-11 17:34:40 +0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,34 +1,23 @@
 import {MnistData} from './data.js';
 
-const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
-
-function doPrediction(model, data, testDataSize = 500) {
+async function doPrediction(model, data, testDataSize = 3) {
   const IMAGE_WIDTH = 28;
   const IMAGE_HEIGHT = 28;
   const testData = data.nextTestBatch(testDataSize);
   const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
-  const labels = testData.labels.argMax(-1);
-  const preds = model.predict(testxs).argMax(-1);
-
+  const labels = testData.labels;
+  const preds = model.predict(testxs);
+  console.log(preds.data());//error:Uncaught TypeError: tf.util.convertBackendValuesAndArrayBuffer is not a function
+  
   testxs.dispose();
-  return [preds, labels];
-}
-
-async function showAccuracy(model, data) {
-  const [preds, labels] = doPrediction(model, data);
-  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
-  const container = {name: 'Accuracy', tab: 'Evaluation'};
-  tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
-
   labels.dispose();
+  preds.dispose();
 }
 
-
-// tf.setBackend('webgl');
 await tf.setBackend('webgpu');
 
 const data = new MnistData();
 await data.load();
 
 const model = await tf.loadLayersModel('./tfmodel.json');
-await showAccuracy(model,data);
+await doPrediction(model,data);
